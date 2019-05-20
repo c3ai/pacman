6cb9da2a317682ffd7beb90e5ce75ec72631646a
Blake Howell <blake.howell@c3.ai>
2019-05-20 12:13:30 -0700

    add boilerplate hackerrank.yml and score.sh

---
+++ score.sh
@@ -0,0 +1,3 @@
+echo "This custom script can run any bash command, and perform tests."
+echo "It needs to only output on1 line in the format 'FS_SCORE:xx%', where xx is the percentage score for the solution."
+echo "FS_SCORE:70%" 
